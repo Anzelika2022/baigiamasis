3934fa6c2e28ec74b9e1f8f3f77991fa632d9269
Anzelika Savickiene <anzelika@savickiene@codeacademylt.onmicrosoft.com>
2023-03-02 11:25:49 +0200

    pirmas komitas

---
+++ laravel_failai/resources/views/layout/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- head section code -->
+</head>
+<body>
+<div id="app">
+    @yield('content')
+</div>
+</body>
+</html>
